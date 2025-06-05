c48e6301032f0c0cbca35b95d4105b81e793821d
arun <arundd2004@gmail.com>
2025-06-05 09:41:52 +0500

    Added Experiment for COD10K

--- example_encoded_image.py
+++ example_encoded_image.py
@@ -25,6 +25,5 @@ concept_heatmaps = pipeline_output.concept_heatmaps
 #print(f"############# Concept heatmaps: {type(concept_heatmaps)}, tensor shape: {concept_heatmaps[0].shape}#########################")
 
 for concept, concept_heatmap in zip(concepts, concept_heatmaps):
-    concept_heatmap_binary = (concept_heatmap > 0.5).float()
-    concept_heatmap_binary.save(f"arun: {concept}.png")
+    concept_heatmap.save(f"arun: {concept}.png")
 
